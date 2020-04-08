e8a6d53c817bcea63961f8e2e7c390787717ffdd
Vishvaka Ranasinghe <vishvaka93@live.com>
2020-04-08 23:26:08 +0530

    Added currencies entity

---
+++ src/PFMS.Domain/Models/Currencies/Currency.cs
@@ -0,0 +1,25 @@
+﻿namespace PFMS.Domain.Models.Currencies
+{
+    public class Currency : Entity
+    {
+        public Currency(string name, string currencyCode, string symbol)
+        {
+            Name = name;
+            ISOCurrencyCode = currencyCode;
+            Symbol = symbol;
+        }
+
+        private Currency()
+        {
+            Name = string.Empty;
+            ISOCurrencyCode = string.Empty;
+            Symbol = string.Empty;
+        }
+
+        public string Name { get; set; }
+
+        public string ISOCurrencyCode { get; set; }
+
+        public string Symbol { get; set; }
+    }
+}
---
+++ src/PFMS.Persistence/EntityConfigurations/Currencies/CurrencyConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PFMS.Domain.Models.Currencies;
+
+namespace PFMS.Persistence.EntityConfigurations.Currencies
+{
+    public class CurrencyConfiguration : IEntityTypeConfiguration<Currency>
+    {
+        public void Configure(EntityTypeBuilder<Currency> builder)
+        {
+            builder.ToTable("Currencies");
+
+            builder.HasKey(i => i.Id);
+
+            builder
+                .Property(i => i.Name)
+                .IsRequired();
+
+            builder
+                .Property(i => i.ISOCurrencyCode)
+                .IsRequired();
+
+            builder
+                .Property(i => i.Symbol)
+                .IsRequired();
+
+            builder
+                .HasIndex(i => i.ISOCurrencyCode)
+                .IsUnique();
+        }
+    }
+}
