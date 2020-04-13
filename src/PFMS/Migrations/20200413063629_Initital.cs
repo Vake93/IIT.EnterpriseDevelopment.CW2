6193973ebfcbe50f95b18660a76166de4e099fe1
Vishvaka Ranasinghe <vishvaka93@live.com>
2020-04-13 12:06:55 +0530

    Added new migration

---
+++ src/PFMS/Migrations/20200413063629_Initital.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PFMS.Migrations
+{
+    public partial class Initital : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Currencies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Modified = table.Column<DateTime>(nullable: false),
+                    Deleted = table.Column<bool>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    ISOCurrencyCode = table.Column<string>(nullable: false),
+                    Symbol = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Currencies", x => x.Id);
+                    table.UniqueConstraint("AK_Currencies_ISOCurrencyCode", x => x.ISOCurrencyCode);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExchangeRates",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Modified = table.Column<DateTime>(nullable: false),
+                    Deleted = table.Column<bool>(nullable: false),
+                    EffectiveDate = table.Column<DateTime>(nullable: false),
+                    FromISOCurrencyCode = table.Column<string>(nullable: true),
+                    ToISOCurrencyCode = table.Column<string>(nullable: true),
+                    Rate = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExchangeRates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ExchangeRates_Currencies_FromISOCurrencyCode",
+                        column: x => x.FromISOCurrencyCode,
+                        principalTable: "Currencies",
+                        principalColumn: "ISOCurrencyCode",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ExchangeRates_Currencies_ToISOCurrencyCode",
+                        column: x => x.ToISOCurrencyCode,
+                        principalTable: "Currencies",
+                        principalColumn: "ISOCurrencyCode",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Modified = table.Column<DateTime>(nullable: false),
+                    Deleted = table.Column<bool>(nullable: false),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    DefaultISOCurrencyCode = table.Column<string>(nullable: true),
+                    SecurityStamp = table.Column<string>(nullable: true),
+                    UserName = table.Column<string>(nullable: true),
+                    PasswordHash = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Currencies_DefaultISOCurrencyCode",
+                        column: x => x.DefaultISOCurrencyCode,
+                        principalTable: "Currencies",
+                        principalColumn: "ISOCurrencyCode",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BankAccounts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Modified = table.Column<DateTime>(nullable: false),
+                    Deleted = table.Column<bool>(nullable: false),
+                    BankName = table.Column<string>(nullable: true),
+                    AccountName = table.Column<string>(nullable: true),
+                    BankAccountType = table.Column<string>(nullable: false),
+                    ISOCurrencyCode = table.Column<string>(nullable: true),
+                    Balance = table.Column<decimal>(nullable: false),
+                    InterestRate = table.Column<decimal>(nullable: false),
+                    UserId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BankAccounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BankAccounts_Currencies_ISOCurrencyCode",
+                        column: x => x.ISOCurrencyCode,
+                        principalTable: "Currencies",
+                        principalColumn: "ISOCurrencyCode",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_BankAccounts_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExpenseSources",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Modified = table.Column<DateTime>(nullable: false),
+                    Deleted = table.Column<bool>(nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    UserId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExpenseSources", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ExpenseSources_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IncomeSources",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Modified = table.Column<DateTime>(nullable: false),
+                    Deleted = table.Column<bool>(nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    UserId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IncomeSources", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_IncomeSources_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Expenses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Modified = table.Column<DateTime>(nullable: false),
+                    Deleted = table.Column<bool>(nullable: false),
+                    ExpenseSourceId = table.Column<Guid>(nullable: false),
+                    ISOCurrencyCode = table.Column<string>(nullable: true),
+                    Amount = table.Column<decimal>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Expenses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Expenses_ExpenseSources_ExpenseSourceId",
+                        column: x => x.ExpenseSourceId,
+                        principalTable: "ExpenseSources",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Expenses_Currencies_ISOCurrencyCode",
+                        column: x => x.ISOCurrencyCode,
+                        principalTable: "Currencies",
+                        principalColumn: "ISOCurrencyCode",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Income",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Modified = table.Column<DateTime>(nullable: false),
+                    Deleted = table.Column<bool>(nullable: false),
+                    IncomeSourceId = table.Column<Guid>(nullable: false),
+                    ISOCurrencyCode = table.Column<string>(nullable: true),
+                    Amount = table.Column<decimal>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Income", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Income_Currencies_ISOCurrencyCode",
+                        column: x => x.ISOCurrencyCode,
+                        principalTable: "Currencies",
+                        principalColumn: "ISOCurrencyCode",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Income_IncomeSources_IncomeSourceId",
+                        column: x => x.IncomeSourceId,
+                        principalTable: "IncomeSources",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankAccounts_ISOCurrencyCode",
+                table: "BankAccounts",
+                column: "ISOCurrencyCode");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankAccounts_UserId",
+                table: "BankAccounts",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExchangeRates_ToISOCurrencyCode",
+                table: "ExchangeRates",
+                column: "ToISOCurrencyCode");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExchangeRates_FromISOCurrencyCode_ToISOCurrencyCode_EffectiveDate",
+                table: "ExchangeRates",
+                columns: new[] { "FromISOCurrencyCode", "ToISOCurrencyCode", "EffectiveDate" },
+                unique: true,
+                filter: "[FromISOCurrencyCode] IS NOT NULL AND [ToISOCurrencyCode] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Expenses_ExpenseSourceId",
+                table: "Expenses",
+                column: "ExpenseSourceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Expenses_ISOCurrencyCode",
+                table: "Expenses",
+                column: "ISOCurrencyCode");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExpenseSources_UserId",
+                table: "ExpenseSources",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Income_ISOCurrencyCode",
+                table: "Income",
+                column: "ISOCurrencyCode");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Income_IncomeSourceId",
+                table: "Income",
+                column: "IncomeSourceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IncomeSources_UserId",
+                table: "IncomeSources",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_DefaultISOCurrencyCode",
+                table: "Users",
+                column: "DefaultISOCurrencyCode");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserName",
+                table: "Users",
+                column: "UserName",
+                unique: true,
+                filter: "[UserName] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BankAccounts");
+
+            migrationBuilder.DropTable(
+                name: "ExchangeRates");
+
+            migrationBuilder.DropTable(
+                name: "Expenses");
+
+            migrationBuilder.DropTable(
+                name: "Income");
+
+            migrationBuilder.DropTable(
+                name: "ExpenseSources");
+
+            migrationBuilder.DropTable(
+                name: "IncomeSources");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Currencies");
+        }
+    }
+}
