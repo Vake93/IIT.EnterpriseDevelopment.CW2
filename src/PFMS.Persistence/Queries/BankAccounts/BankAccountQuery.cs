6c731c2b9eee6de026a507178369ea306b291907
Vishvaka <vishvaka@calcey.com>
2020-04-16 17:14:23 +0530

    Update BankAccountQuery.cs

--- src/PFMS.Persistence/Queries/BankAccounts/BankAccountQuery.cs
+++ src/PFMS.Persistence/Queries/BankAccounts/BankAccountQuery.cs
@@ -1,5 +1,11 @@
-﻿using PFMS.Domain.Models.BankAccounts;
+﻿using Microsoft.EntityFrameworkCore;
+using PFMS.Domain.Models.BankAccounts;
 using PFMS.Domain.Queries.BankAccounts;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace PFMS.Persistence.Queries.BankAccounts
 {
@@ -8,6 +14,17 @@ namespace PFMS.Persistence.Queries.BankAccounts
         public BankAccountQuery(DatabaseContext context)
             : base(context)
         {
+        }
+
+        public Task<decimal> GetTotalAsync(
+            Expression<Func<BankAccount, bool>> predicate,
+            CancellationToken cancellationToken = default)
+        {
+            return Context
+                .Set<BankAccount>()
+                .AsNoTracking()
+                .Where(predicate)
+                .SumAsync(i => i.Balance, cancellationToken);
         }
     }
 }
